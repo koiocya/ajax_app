1aed30f81fe152b8abdc84841db51c3e1b32b2cc
koiocya <73815463+koiocya@users.noreply.github.com>
2020-11-13 12:45:46 +0900

    メモ投稿機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,7 @@ class PostsController < ApplicationController
   end
   def create
     post = Post.create(content: params[:content], checked: false)
-    render json: {post: post}
-    redirect_to action: :index
+    render json:{ post: post }
   end
   def checked
     post = Post.find(params[:id])
